67e84416eb46a27dedce4e44471a431cf11e3953
muneebusmani <muneebusmani8355@gmail.com>
2024-06-04 04:38:21 +0500

    created a generic plugin repo

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,3 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
--- local map = vim.keymap.set
 ---@param mode string Vim Mode for the mapping
 ---@param lhs string Keybinding for the mapping
 ---@param rhs string Target command(lua,vimscript)
